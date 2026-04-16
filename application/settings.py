6c5812121a4f89cf6beefbcf06c95509042591c8
GrayMouse9 <zenacederbaum@gmail.com>
2026-04-16 19:03:27 +0300

    New

--- application/settings.py
+++ application/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,0.0.0.0").split(",")
+
 
 
 # Application definition
