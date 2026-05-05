357033660ca76c09b115678842018294ab644632
GrayMouse9 <129786887+GrayMouse9@users.noreply.github.com>
2026-05-05 12:11:06 +0300

    hw-2: views, шаблоны, пагинация, Docker, окружение
    - Реализованы view для всех страниц (index, hot, tag, question, ask, login, signup, profile)
       - Настроены урлы и именованные маршруты
       - Шаблоны переведены на extends + include, добавлен паттерн пагинации
       - Создан requirements.txt, .gitignore, .env.example
       - Добавлен Dockerfile и docker-compose.yml

--- application/settings.py
+++ application/settings.py
@@ -24,10 +24,11 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,0.0.0.0").split(",")
+
+
 
 # Application definition
 
--- questions/urls.py
+++ questions/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('hot/', views.hot, name='hot'),
     path('question/<int:question_id>/', views.question, name='question'),
     path('ask/', views.ask, name='ask'),
     path('tag/<str:tag_name>/', views.tag, name='tag'),
